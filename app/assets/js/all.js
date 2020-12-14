886a22f8edbcf7b42b4731f97ea765fa90d5f9a0
SuPan <joelopan@gmail.com>
2020-12-15 01:04:18 +0800

    shop

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,17 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+  $('.btn-product-favorite ').click(function(){
+    if($('.material-icons-favorite').hasClass('text-primary')) {
+      $('.material-icons-favorite').addClass('text-hot')
+      $('.material-icons-favorite').removeClass('text-primary')
+    }else{
+      $('.material-icons-favorite').addClass('text-primary')
+      $('.material-icons-favorite').removeClass('text-hot')
+    }
+
+  });
+
+
 });
