1e4f191ec4128b0595f63cfc29f726ed056a0e61
SuPan <joelopan@gmail.com>
2020-12-17 17:15:53 +0800

    aos

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,8 +11,17 @@ $(document).ready(() => {
       $('.material-icons-favorite').addClass('text-primary')
       $('.material-icons-favorite').removeClass('text-hot')
     }
-
   });
 
+  AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+
+    
+  });
 
 });
+
