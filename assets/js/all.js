63c14dc392c600dad70b332921a86fe8821eb89a
SuPan <joelopan@gmail.com>
2020-12-17 17:17:05 +0800

    Update 2020-12-17T09:16:58.755Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -12,5 +12,15 @@ $(document).ready(function () {
       $('.material-icons-favorite').removeClass('text-hot');
     }
   });
+  AOS.init({
+    offset: 120,
+    // offset (in px) from the original trigger point
+    delay: 0,
+    // values from 0 to 3000, with step 50ms
+    duration: 1000,
+    // values from 0 to 3000, with step 50ms
+    anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+  });
 });
 //# sourceMappingURL=all.js.map
